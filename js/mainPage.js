038d6d8407dd1a2897f5410a6bcbdd24d09fb672
jysdu2006 <jysdu2006@gmail.com>
2014-11-18 10:53:06 +0800

    20141118 10:53

--- js/mainPage.js
+++ js/mainPage.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 
+	$('.modal.fade').load('http://sdumicrohtml.qiniudn.com/regester.html');
+
 	$("#logopic").hover(function(){
 		$("#picname").fadeIn("slow");
 	},
@@ -16,14 +18,24 @@ $(document).ready(function(){
 
 
 	$("#login").click(function(){
-		alert("test");
+		
 	});
 
 	$('.shCont').click(function(){
-		//var page='html/'+$(this).data('page')+'.html';
+		//var page='../../html/'+$(this).data('page')+'.html';
 		var page='http://sdumicrohtml.qiniudn.com/'+$(this).data('page')+'.html';
 		alert(page);
 		$('.content').load(page);
 	});
+
+
+
+
+	//var c=document.getElementById("secondCanvas");
+	var c=$('#secondCanvas').get(0);
+	var ctx=c.getContext("2d");
+	ctx.moveTo(200,10);
+	ctx.lineTo(1100,10);
+	ctx.stroke();
 
 });
